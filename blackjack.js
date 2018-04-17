80439a5bf38f70819589445acb770f538fe1b38e
leimapapa <36284756+leimapapa@users.noreply.github.com>
2018-04-16 23:41:05 -0600

    Update blackjack.js
    Add getRandom function to make blackjack function work

--- blackjack.js
+++ blackjack.js
@@ -1,5 +1,13 @@
 //blackjack
 
+function getRandom(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min; 
+	//The maximum is inclusive and the minimum is inclusive 
+	}
+
+
 function blackjack(card){
 
 	var randomNumber = getRandom(1,52);
